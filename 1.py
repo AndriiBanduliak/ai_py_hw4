5594c0994a2464ebf8f8c3c926e6067fa099b501
Banduliak Andrii <109424920+AndriiBanduliak@users.noreply.github.com>
2022-12-19 14:20:22 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,3 +1,6 @@
-from math import pi
+k = 1
+x = 0
+for k in range(1, 1000000):
+    x = x + 4 * ((-1)**(k + 1)) / (2 * k - 1)
 
-print(round(pi, int(input("Задайте число знаков после запятой: "))))
+print(round(x, int(input("Задайте число знаков после запятой: "))))
